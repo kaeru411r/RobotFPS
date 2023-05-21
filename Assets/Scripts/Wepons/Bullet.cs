9baaae0e2009bd6787c469aefdc7e0e060ead682
kaeru411r <ike269fr@gmail.com>
2023-05-22 02:18:15 +0900

    銃を撃てるように

---
+++ Assets/Scripts/Wepons/Bullet.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// íe
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    const int _enemyLayer = 8;
+    const int _bulletLayer = 9;
+
+    int _atk = 0;
+    float _speed = 0;
+    Rigidbody _rb;
+    bool _isFire;
+
+    /// <summary>çUåÇóÕ</summary>
+    public int Atk { get => _atk; set => _atk = value; }
+    /// <summary>íeë¨</summary>
+    public float Speed { get => _speed; set => _speed = value; }
+    public Rigidbody RigidBody { get => _rb; set => _rb = value; }
+
+    private void Awake()
+    {
+        gameObject.layer = _bulletLayer;
+        _rb = GetComponent<Rigidbody>();
+        _rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+    }
+
+    /// <summary>
+    /// î≠éÀä÷êî
+    /// </summary>
+    /// <param name="atk"></param>
+    /// <param name="speed"></param>
+    public void Fire(int atk, float speed, float lifeTime)
+    {
+        _atk = atk;
+        _speed = speed;
+        _rb.velocity = transform.forward * speed;
+        _isFire = true;
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Hit(Collision hit)
+    {
+        if (hit.gameObject.layer == _enemyLayer)
+        {
+            Debug.Log("Hit");
+        }
+
+        Destroy(gameObject);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_isFire && collision.gameObject.layer != _bulletLayer)
+        {
+            Hit(collision);
+        }
+    }
+}
--- Assets/Scripts/Wepons/Gun.cs
+++ Assets/Scripts/Wepons/Gun.cs
@@ -7,12 +7,16 @@ public class Gun : WeponBase
 {
     [SerializeField, Tooltip("UŒ‚—Í")]
     int _atk;
+    [SerializeField, Tooltip("’e‘¬")]
+    float _speed;
     [SerializeField, Tooltip("”­ŽËƒŒ[ƒg")]
     float _fireRate;
     [SerializeField, Tooltip("ƒŠƒ[ƒhŽžŠÔ")]
     float _reloadTime;
     [SerializeField, Tooltip("ŽËŒ‚ƒ‚[ƒh")]
     FireMode _fireMode;
+    [SerializeField, Tooltip("’e")]
+    Bullet _bullet;
     [SerializeField, Tooltip("eŒû")]
     Transform _muzzle;
 
@@ -21,6 +25,7 @@ public class Gun : WeponBase
     bool _isAiming = false;
     bool _isPause = false;
     float _fireTime = 0;
+    Rigidbody _rb;
 
     public FireMode Firetype { get => _fireMode; set => _fireMode = value; }
 
@@ -37,7 +42,7 @@ public class Gun : WeponBase
 
     public override void OnAim(WeponActionPhase phase)
     {
-        if(_isPause) { return; }
+        if (_isPause) { return; }
         if (phase == WeponActionPhase.Started)
         {
             _isAiming = true;
@@ -68,6 +73,12 @@ public class Gun : WeponBase
         {
             StartCoroutine(Reload());
         }
+    }
+
+    public override void Attach(RobotBase robot)
+    {
+        base.Attach(robot);
+        _rb = robot.GetComponent<Rigidbody>();
     }
 
     private void Start()
@@ -106,16 +117,16 @@ public class Gun : WeponBase
 
     IEnumerator Reload()
     {
-        if(_isReloading) { yield break; }
+        if (_isReloading) { yield break; }
         Debug.Log("Reloading");
         _isReloading = true;
         float time = _reloadTime;
         while (true)
         {
             yield return null;
-            if(_isPause) { continue; }
+            if (_isPause) { continue; }
             time -= Time.deltaTime;
-            if(time < 0)
+            if (time < 0)
             {
                 _isReloading = false;
                 Debug.Log("Reloaded");
@@ -126,7 +137,16 @@ public class Gun : WeponBase
 
     void Fire()
     {
-        Debug.Log("Fire");
+        if (_bullet && _muzzle)
+        {
+            var bullet = Instantiate(_bullet, _muzzle.position, _muzzle.rotation);
+            if (_rb)
+            {
+                bullet.RigidBody.velocity = _rb.velocity;
+            }
+            bullet.Fire(_atk, _speed, 10);
+            Debug.Log("Fire");
+        }
     }
 
     public enum FireMode
